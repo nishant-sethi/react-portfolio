cfa36af70dff6b0f31b3d5c56bfda14d984dbc82
nishantsethi <nsethi610@gmail.com>
2026-04-13 20:31:55 +0100

    modify exports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
-import Testimonials from "./Components/Testimonials";
-import Portfolio from "./Components/Portfolio";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import {
+  Header,
+  Footer,
+  About,
+  Resume,
+  Contact,
+  Testimonials,
+  Portfolio,
+} from './Components';
 
 const App = () => {
   const [resumeData, setResumeData] = useState({});
 
   useEffect(() => {
-    fetch("/resumeData.json")
+    fetch('/resumeData.json')
       .then((res) => res.json())
       .then((data) => setResumeData(data))
       .catch((err) => {
@@ -22,7 +24,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header data={resumeData.main} />
       <About data={resumeData.main} />
       <Resume data={resumeData.resume} />
